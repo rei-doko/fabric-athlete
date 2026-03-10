7e1cee50f6b19e2db42980152476010bccc7afa9
rei.doko <jr.danas23@gmail.com>
2026-03-11 07:51:09 +0800

    Reorganized file structure and updated url links

---
+++ script/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const tabs=document.querySelectorAll('.tab-btn');
+	const contents=document.querySelectorAll('.settings-wrapper');
+
+	tabs.forEach(tab=>{
+		tab.addEventListener('click',()=>{
+			tabs.forEach(t => t.classList.remove('active'));
+			tab.classList.add('active');	
+	
+			contents.forEach(c => c.classList.remove('active'));
+			const target = document.getElementById(tab.dataset.target);
+			target.classList.add('active');
+		});
+	});
+});
